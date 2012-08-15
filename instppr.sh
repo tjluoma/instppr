4c4ded018417d53ba1ce656e854d9e0d272e5440
TJ Luoma <luomat@gmail.com>
2012-08-15 18:05:42 -0400

    placeholder for shell script

---
+++ instppr.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+# A zsh shell script for working with the Instapaper API
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# CDate:	2012-08-15
+
+NAME="$0:t"
+
+echo "This will do something once I get permission to use the API. See http://www.instapaper.com/main/request_oauth_consumer_token and http://www.instapaper.com/api/full/ for details"
+
+
+exit 0
+
+#
+#EOF
